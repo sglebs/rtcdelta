42b5abab20e13beda24850b472ee31ecca5057d9
Marcio Marchini <mqm@sglebs.com>
2016-03-16 10:18:22 -0300

    prints changesets for a work-item

--- rtcdelta.py
+++ rtcdelta.py
@@ -1,13 +1,16 @@
 """RTC Delta
 
 Usage:
-  rtcdelta      --user=<username> --pass=<password> --url=<rtcUrl> \r\n \
-                [--endsWithJazz]
+  rtcdelta      --user=<username> --pass=<password> --url=<rtcUrl> --workitem=<id>  \r\n \
+                [--endsWithJazz] [--logs]
 
 Options:
   --user=<username>         Username, for RTC authentication.
   --pass=<password>         Password, for RTC authentication.
   --url=<rtcUrl>            RTC URL.
+  --workitem=<id>           ID of the work item we want to analyze.
+  --endsWithJazz            If your url ends with ccm, no neet to set to True.
+  --logs                    If you want to see log lines in the console
 
 
 Author:
@@ -15,21 +18,22 @@ Author:
 
 """
 
-
 from docopt import docopt
-from rtcclient.utils import setup_basic_logging
 from rtcclient.client import RTCClient
+from rtcclient.utils import setup_basic_logging
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='RTC Delta')
-    # you can remove this if you don't need logging
-    # default debug logging for console output
-    setup_basic_logging()
-    # if your url ends with ccm, set ends_with_jazz to False
-    # refer to issue #68 for detailed explanation
-    rtcClient = RTCClient(arguments["--url"],
+    if arguments["--logs"]:
+        setup_basic_logging()
+    rtc_client = RTCClient(arguments["--url"],
                           arguments["--user"],
                           arguments["--pass"],
                           ends_with_jazz=arguments["--endsWithJazz"])
-    prj_areas = rtcClient.getProjectAreas()
-    print (prj_areas)
+    #prj_areas = rtc_client.getProjectAreas()
+    #print (prj_areas)
+    work_item = rtc_client.getWorkitem(arguments["--workitem"]) # ,returned_properties="dc.ientifier,dc:title"
+    print("%s : %s"% (work_item.identifier, work_item.title))
+    changesets = work_item.getChangeSets()
+    for changeset in changesets:
+        print (changeset)
