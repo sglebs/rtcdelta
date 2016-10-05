f799c14501e7712a97e908e87ee12e8d3a7ae832
Felipe Ruhland <felipe.ruhland@gmail.com>
2016-10-05 09:38:42 -0300

    Fix pep8 issues

--- rtcdelta.py
+++ rtcdelta.py
@@ -1,7 +1,8 @@
 """RTC Delta
 
 Usage:
-  rtcdelta      --user=<username> --pass=<password> --url=<rtcUrl> --workitem=<id>  --beforePath=<beforePath> --afterPath=<afterPath> \r\n \
+  rtcdelta      --user=<username> --pass=<password> --url=<rtcUrl> --workitem=<id> \
+                --beforePath=<beforePath> --afterPath=<afterPath> \r\n \
                 [--endsWithJazz] [--logs] [--children]
 
 Options:
@@ -27,54 +28,60 @@ from rtcclient.client import RTCClient
 from rtcclient.utils import setup_basic_logging
 import os.path
 
+
 def ensure_path_exists(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
 
-def get_changes(changesets):
+def get_changes(workitem):
+    print('{identifier}: {title}'.format(identifier=workitem.identifier, title=workitem.title))
+    changesets = workitem.getChangeSets()
+
     if not changesets:
         return
 
-    for changeset in sorted(changesets, key=lambda changeset: changeset['modified']):
-        print("Traversing %s" % changeset)
-        for change in sorted(changeset.getChanges(), key=lambda change: change['modified'])
-            print("Extracting before&after for change: %s" % change.comment)
+    for changeset in changesets:
+        print('  Traversing {changeset}'.format(changeset=changeset))
+        for change in changeset.getChanges():
+            print('    Extracting before&after for change: %s' % change.comment)
             change.fetchBeforeStateFile(arguments["--beforePath"])
             change.fetchAfterStateFile(arguments["--afterPath"])
 
 
 def get_workitem_list(workitem):
-    print("Getting workitem list from %s : %s"% (workitem.identifier, workitem.title))
+    print('Getting workitem list from {identifier}: {title}'.format(
+        identifier=workitem.identifier,
+        title=workitem.title
+    ))
     workitem_list = [workitem]
     children_list = workitem.getChildren()
     if not children_list:
         return workitem_list
 
     for child in children_list:
-        workitem_list += get_children(child)
+        workitem_list += get_workitem_list(child)
 
     return workitem_list
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='RTC Delta')
-    if arguments["--logs"]:
+    if arguments['--logs']:
         setup_basic_logging()
     else:
         logging.basicConfig(level=logging.CRITICAL)
 
-    rtc_client = RTCClient(arguments["--url"],
-                          arguments["--user"],
-                          arguments["--pass"],
-                          ends_with_jazz=arguments["--endsWithJazz"])
-    ensure_path_exists(arguments["--beforePath"])
-    ensure_path_exists(arguments["--afterPath"])
-    workitem = rtc_client.getWorkitem(arguments["--workitem"]) # ,returned_properties="dc.ientifier,dc:title,rtc_cm:com.ibm.team.filesystem.workitems.change_set.com.ibm.team.scm.ChangeSet"
+    rtc_client = RTCClient(arguments['--url'],
+                           arguments['--user'],
+                           arguments['--pass'],
+                           ends_with_jazz=arguments['--endsWithJazz'])
+
+    ensure_path_exists(arguments['--beforePath'])
+    ensure_path_exists(arguments['--afterPath'])
+    workitem = rtc_client.getWorkitem(arguments['--workitem'])
     if arguments['--children']:
         for child in get_workitem_list(workitem):
-            print("%s : %s"% (child.identifier, child.title))
-            get_changes(child.getChangeSets())
+            get_changes(child)
     else:
-        print("%s : %s"% (workitem.identifier, workitem.title))
-        get_changes(workitem.getChangeSets())
+        get_changes(workitem)
